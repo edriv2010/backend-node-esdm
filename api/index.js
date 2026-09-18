6f4e856174786950f03234c2f732f615afdabb0e
edwinj <edwin.junaidi@ogya.co.id>
2026-09-18 18:24:42 +0700

    Perubahan api/index

--- api/index.js
+++ api/index.js
@@ -47,7 +47,7 @@ app.get("/",(req,res)=>res.json({message:"ESDM Backend LIVE ssss ✅", SHEET_ID,
 app.get("/api/sheet", async (req,res)=>{
   try{
     const csvUrl = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv&gid=${EN_LOCAL_GID}`;
-    res.json("csvUrl":csvUrl);
+    res.json({"csvUrl":csvUrl});
 	const r = await fetch(csvUrl);
     if(!r.ok) throw new Error("Gagal fetch sheet: "+r.status);
     const csv = await r.text();
